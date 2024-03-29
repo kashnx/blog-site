93c92e0b1ca0912c6832866bb9003315ab7d073f
kashnx <kashnii250@gmail.com>
2024-03-29 16:48:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
